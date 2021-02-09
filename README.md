10fe4ebfa7e2bf7ab3c01df0f167e78a39165eb9
resres2007@gmail.com <30293380+AndusDEV@users.noreply.github.com>
2021-02-09 11:08:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YT-Music-TV
+ Basically... YouTube Music for TV. Not that great as original google version... But there are no ads!
---
+++ app/src/main/java/pl/andus/ytmusic/tv/FullscreenActivity.java
@@ -0,0 +1,37 @@
+package pl.andus.ytmusic.tv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class FullscreenActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://music.youtube.com/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
